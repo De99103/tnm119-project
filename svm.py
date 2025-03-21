12225065c41816ca1b78ced6b9692e72da372670
Majsburken <118824234+Majsburken@users.noreply.github.com>
2025-03-21 10:10:30 +0100

    added randomForest and svm
    believed to be done, need to be reviewed :)

---
+++ svm.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.svm import SVC
+
+# Load CSV files into DataFrames
+df_fake = pd.read_csv("Fake.csv")
+df_real = pd.read_csv("True.csv")
+
+# Display DataFrames
+# print("Fake News DataFrame (head):")
+# print(df_fake.head())
+
+# print("\nReal News DataFrame (head):")
+# print(df_real.head())
+
+# Lägg till en kolumn med etikett (label)
+df_fake['label'] = 'Fake'  # Fake news får etikett 0
+df_real['label'] = 'True'  # Real news får etikett 1
+
+# Concatenate the fake and real datasets into one combined DataFrame
+df = pd.concat([df_fake, df_real], ignore_index=True)
+
+df["processed_text"] = df["title"] + " " + df["text"]
+df["processed_text"] = df["processed_text"].str.lower()
+
+# Fix the column names to include the correct columns and the new 'label'
+df.columns = ["title", "text", "subject", "date", "label", "processed_text"]
+
+X = df.iloc[:, -1]
+Y = df.iloc[:, -2]
+# print(X)
+# print (Y)
+
+# Split words and explode DataFrame to have each word in its own row
+df_words = df.assign(word=df["processed_text"].str.split()).explode("word")
+
+# Visa resultatet
+# print(df_words[["word", "label"]].head())
+
+seed = 42
+np.random.seed(seed)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20, random_state=seed)
+
+print(f"Testing sampels: {len(X_train)}, Testing samples : {len(X_test)}")
+
+vectorizer = TfidfVectorizer(max_features=5000, stop_words='english')
+
+# TF_IDF = Varje ord viktas beroende på hur viktigt och ovanligt ordet är.
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+#2 FITTING A MODEL TO THE TRAINING DATA
+#linear boundary to separate classes, high C -> complex might not classify unseen data correctly, low C -> better performance
+svm_clf = SVC(kernel='linear', C=0.1)
+
+#4 TRANSFORMING THE TEST DATA TO FIND DECISION BOUNDARY
+#finds the best location for the linear boundary
+svm_clf.fit(X_train_tfidf, Y_train)
+
+Y_pred = svm_clf.predict(X_test_tfidf)
+
+# Print Accuracy
+accuracy = accuracy_score(Y_test, Y_pred)
+print(f"SVM Accuracy: {accuracy:.4f}")
+
+# Print Classification Report
+print(classification_report(Y_test, Y_pred))
